816a07bcf7369466ace000098e254305a774c731
Martin Pernecky <mpe@auctionata.com>
2016-03-06 03:23:35 +0100

    configurable port

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ let app = express();
 app.use(express.static('public'));
 app.get('/stream', stream.init);
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 
 function transformParty(parties) {
   var x = _(parties)
